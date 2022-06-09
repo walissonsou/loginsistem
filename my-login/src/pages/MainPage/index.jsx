d99aa46bc66ed98dce11518b8786df6571bb73b7
walissonsou <walissonsouza96@hotmail.com>
2022-06-09 13:35:03 -0300

    tag carregando dados

--- my-login/src/pages/MainPage/index.jsx
+++ my-login/src/pages/MainPage/index.jsx
@@ -1,21 +1,31 @@
 import React, {useState, useEffect} from 'react';
+
+import {Link} from 'react-router-dom'
+
 import Nav from './components/Nav';
 import Repo from './components/Repositories';
 import Search from './components/Search';
 import "./styles.css";
 import { getRepositories } from '../../services/api'
 
-const userId = '62a0b724ef09380b56077a27'
+const userId = '62a0ee7973eced073d144cd8'
 
 const MainPage = () => {
   const [repositories, setRepositories] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [loadingError, setLoadingError] = useState(false);
 
   const loadData = async (query = '') => {
-    const response = await getRepositories(userId);
-
-    console.log(response.data)
-
-    setRepositories(response.data)
+    try{
+      setLoading(true)
+      const response = await getRepositories(userId); 
+      setRepositories(response.data)
+      setLoading(false)
+    } catch(err) {
+      console.error(err);
+      setLoadingError(true)
+    }
+    
   }
 
   useEffect(() => {
@@ -38,6 +48,20 @@ const MainPage = () => {
   }
   const handleNewRepo = (url) => {
     console.log('new', url)
+  }
+  if(loadingError) {
+    return (
+      <div className="loading">
+      Erro ao carregar dados... <Link to='/login'> Voltar </Link>
+    </div>
+    )
+  }
+  if (loading) {
+    return(
+      <div className="loading">
+        Tá carregando padrin... 
+      </div>
+    )
   }
 
   return (
